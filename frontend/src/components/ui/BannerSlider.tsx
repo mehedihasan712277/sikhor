13047b7f2a351e6b3bf9ecff254c72a5befd2f4a
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-07-10 12:42:24 +0600

    spinneer added

--- frontend/src/components/ui/BannerSlider.tsx
+++ frontend/src/components/ui/BannerSlider.tsx
@@ -8,14 +8,11 @@ import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// import './banner.css';
-
-// import required modules
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BannerImageType } from '@/utils/types';
 import { useQuery } from '@tanstack/react-query';
+import { Box, CircularProgress } from '@mui/material';
 
 const fetchData = (): Promise<BannerImageType[]> => {
     return axios.get("https://sikhor-server0.vercel.app/carousel").then(res => res.data)
@@ -23,13 +20,10 @@ const fetchData = (): Promise<BannerImageType[]> => {
 
 const BannerSlider = () => {
     const { data, isLoading } = useQuery({ queryKey: ["slider-image"], queryFn: fetchData })
-    // const [data, setData] = useState<BannerImageType[] | []>([])
-    // useEffect(() => {
-    //     axios.get("https://sikhor-server0.vercel.app/carousel")
-    //         .then(res => setData(res.data))
-    // }, [])
     if (isLoading) {
-        return <>Loading</>
+        return <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "450px" }}>
+            <CircularProgress />
+        </Box>
     }
     return (
         <>
@@ -47,20 +41,20 @@ const BannerSlider = () => {
                 modules={[Autoplay, EffectFade, Navigation, Pagination]}
                 className="mySwiper w-full h-full"
             >
+
                 {
-                    (data as BannerImageType[]).map(ele => <SwiperSlide key={ele.id} style={{ backgroundPosition: "center", backgroundSize: "cover" }} className='h-[500px]'>
-                        <img src={ele.imgUrl} alt={ele.alt} className='block w-full h-[450px]' />
-                    </SwiperSlide>)
+                    (data as BannerImageType[]).map(ele => {
+
+                        return <>
+
+                            <SwiperSlide key={ele.id} style={{ backgroundPosition: "center", backgroundSize: "cover" }} className='h-[500px]'>
+                                <img src={ele.imgUrl} alt={ele.alt} className='block w-full h-[450px]' />
+                            </SwiperSlide>
+
+                        </>
+                    })
                 }
-                {/* <SwiperSlide style={{ backgroundPosition: "center", backgroundSize: "cover" }} className='h-[500px]'>
-                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" className='block w-full h-[500px]' />
-                </SwiperSlide>
-                <SwiperSlide style={{ backgroundPosition: "center", backgroundSize: "cover" }} className='h-[500px]'>
-                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" className='block w-full h-[500px]' />
-                </SwiperSlide>
-                <SwiperSlide style={{ backgroundPosition: "center", backgroundSize: "cover" }} className='h-[500px]'>
-                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" className='block w-full h-[500px]' />
-                </SwiperSlide> */}
+
             </Swiper>
         </>
     );
