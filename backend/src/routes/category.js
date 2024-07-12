d118afca18a969c291d04e3c581f70ab37861ada
Shishir Halder <s.halder0108@gmail.com>
2024-07-12 10:45:22 +0600

    feat: new sub-category and new-sub-sub-category/categories

--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -41,30 +41,92 @@ category
     }
   });
 
-category.post("/new", async (req, res) => {
-  try {
-    const body = req.body;
+category
+  .post("/new", async (req, res) => {
+    try {
+      const body = req.body;
 
-    const existingCategory = await prisma.category.findUnique({
-      where: { name: body.name },
-    });
+      const existingCategory = await prisma.category.findUnique({
+        where: { name: body.name },
+      });
 
-    if (existingCategory) {
-      return res
-        .status(400)
-        .json({ error: "A category with this name already exists!" });
+      if (existingCategory) {
+        return res
+          .status(400)
+          .json({ error: "A category with this name already exists!" });
+      }
+
+      const newCategory = await prisma.category.create({
+        data: body,
+      });
+
+      res.send(newCategory);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal server error");
     }
+  })
+  .post("/:id/new-sub", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
 
-    const newCategory = await prisma.category.create({
-      data: body,
-    });
+      const newSubCategory = await prisma.category.update({
+        where: { id },
+        data: {
+          subCategories: {
+            push: body,
+          },
+        },
+      });
 
-    res.send(newCategory);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Internal server error");
-  }
-});
+      res.send(newSubCategory);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal server error");
+    }
+  })
+  .post("/:categoryId/:subCategoryName/new-sub-sub", async (req, res) => {
+    try {
+      const { categoryId, subCategoryName } = req.params;
+      const { subSubCategories: newSubSubCategories } = req.body;
+
+      const category = await prisma.category.findUnique({
+        where: { id: categoryId },
+      });
+
+      if (!category) {
+        return res.status(400).json({ message: "Category not found" });
+      }
+
+      // Find the sub-category to update
+      const updatedSubCategories = category.subCategories.map((subCategory) => {
+        if (subCategory.name === subCategoryName) {
+          return {
+            ...subCategory,
+            subSubCategories: [
+              ...subCategory.subSubCategories,
+              ...newSubSubCategories,
+            ],
+          };
+        }
+        return subCategory;
+      });
+
+      // Update the category with the updated sub-categories
+      const updatedCategory = await prisma.category.update({
+        where: { id: categoryId },
+        data: {
+          subCategories: updatedSubCategories,
+        },
+      });
+
+      res.send(updatedCategory);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal server error");
+    }
+  });
 
 category.patch("/update/:id", async (req, res) => {
   try {
